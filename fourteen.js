3e53c87677dc11df10d05b34695da5c1086fb4c5
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 23:32:08 +0600

    Ternary operator

---
+++ fourteen.js
@@ -0,0 +1,18 @@
+// Ternary operator
+
+let age = 4;
+let drink;
+
+if (age >= 5) {
+  drink = "coffee";
+} else {
+  drink = "milk";
+}
+
+console.log(drink);
+
+// ternary operator / conditional operator
+
+let age2 = 3;
+let drink2 = age2 >= 5 ? "coffee" : "milk";
+console.log(drink2);
