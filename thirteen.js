87a6150cab5e72b9cbf6d47a73aa95f7cffb9a4f
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 23:31:06 +0600

    if else condition

---
+++ thirteen.js
@@ -0,0 +1,22 @@
+// if else condition
+
+let age = 17;
+
+if (age >= 18) {
+  console.log("User can play ddlc");
+} else {
+  console.log("User can play mario");
+}
+
+// truthy
+// "abc"
+// 1, -1
+
+let firstName = 0;
+
+if (firstName) {
+  console.log(firstName);
+} else {
+  console.log("firstName is kinda empty");
+}
+// Output is: "FirstName is kinda Empty
