189a4657242892a5a3b91ec318045274b4d712a6
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 14:53:50 +0600

    type_of_operators

---
+++ eight.js
@@ -0,0 +1,29 @@
+// Typeof operator
+
+// Data types (Primitive data types)
+// String: "harhit"
+// Number: 2, 4, 5.6
+// Booleans: True or False
+// Undefined
+// Null
+// BigInt
+// Symbol
+
+let age1 = 22;
+let firstName = "harshit";
+
+console.log(typeof age1);
+console.log(typeof "harshit");
+
+// 22 -> "22"
+// Convert number to string.
+age1 = age1 + "";
+console.log(typeof age1); // "22"
+
+// Convert string to number.
+let myStr = +"34";
+console.log(typeof myStr);
+
+let age2 = "18";
+age2 = Number(age2);
+console.log(typeof age2); // Number
