f4711cd4270e76d8fa9178184c75bfa47ca78548
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 14:55:35 +0600

    Template string

---
+++ ten.js
@@ -0,0 +1,10 @@
+// Template string
+let age = 22;
+let firstName = "adnan";
+
+// "my name is adnan and my age is 22 "
+// let aboutMe = "my name is " + firstName + " and my age is " + age;
+
+let aboutMe = `my name is ${firstName} and my age is ${age}`;
+
+console.log(aboutMe);
