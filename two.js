710e2fb912d539875a3ff723f80ba46fe539a267
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 14:46:21 +0600

    use_strict_&_variable

---
+++ two.js
@@ -0,0 +1,22 @@
+// The `"use strict"` directive in JavaScript is used to enforce stricter parsing and
+// error handling on your code at runtime, helping to catch common coding mistakes
+// and prevent silent errors.
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable
+
+var firstName = "Harshit";
+
+// use a variable
+console.log(firstName);
+
+// change value
+
+firstName = "Mohit";
+
+console.log(firstName);
