473ce1de8b45315500ebfa38f6e8324116bb8c08
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 14:51:03 +0600

    about_string_indexing

---
+++ six.js
@@ -0,0 +1,19 @@
+// String indexing
+
+let firstName = "nuradnan";
+
+//  n    u   r   a   d   n   a
+//  0    1   2   3   4   5   6
+
+// console.log(firstName[0]);
+// length of string
+// firstName.length
+
+console.log(firstName.length);
+
+// To Find the last index of the string
+
+console.log(firstName.length - 1);
+
+// To Find the second last character of the string
+console.log(firstName[firstName.length - 2]);
