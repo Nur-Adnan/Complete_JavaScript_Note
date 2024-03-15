0049eefa50533e759abc266b659a5cced7be7d88
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-03-15 23:33:36 +0600

    nested_if_else

---
+++ fifteen.js
@@ -0,0 +1,24 @@
+var firstName = "Adnan";
+
+if (firstName[0] === "A") {
+  console.log("your name starts with A");
+}
+
+if (age > 18) {
+  console.log("you are above 18");
+}
+
+if (firstName[0] === "A" && age > 18) {
+  console.log("Name starts with A and above 18");
+} else {
+  console.log("inside else");
+}
+
+let firstName = "Nur";
+let age = 16;
+
+if (firstName[0] === "A" || age > 18) {
+  console.log("inside if");
+} else {
+  console.log("inside else");
+}
